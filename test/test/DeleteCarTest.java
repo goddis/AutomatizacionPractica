9d5eb5139db492a73bc2bdd38f8c3b808548c1d5
Yodi Pino <ypino@uccorp.net>
2019-02-26 11:16:40 -0500

    Realizamos un cambio en el archivo delete linea 63,64

--- test/test/DeleteCarTest.java
+++ test/test/DeleteCarTest.java
@@ -60,8 +60,8 @@ public class DeleteCarTest {
         objLogin = new LoginCars(driver);
         objLogin.login("admin@phptravels.com", "demoadmin"); 
 
-        Alert alerta2 = driver.switchTo().alert();
-        alerta2.accept();
+        Alert alerta = driver.switchTo().alert();
+        alerta.accept();
         
         //String ConfirmacionDelete = driver.switchTo().alert().getText() ;
         //Assert.assertTrue(ConfirmacionDelete.contains("Customer deleted Successfully"));
